a7216b3b6669865f75318475c0cb6ce6c84e3905
unknown <you@example.com>
2025-06-11 12:25:25 +0530

    setup basic dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const express = require("express");
+const {PORT} = require('./config/serverConfig.js');
+
+const setupAndStartserver = async ()=>{
+
+    const app = express();
+
+    app.listen(PORT , ()=>{
+        console.log(`server started at ${PORT}`);    
+    });
+
+}
+setupAndStartserver();
