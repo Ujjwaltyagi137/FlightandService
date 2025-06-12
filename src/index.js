f8472f93770527e0e522723e5c58e7b6098fd105
unknown <you@example.com>
2025-06-12 23:13:42 +0530

    added city

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {PORT} = require('./config/serverConfig.js');
 const bodyParser = require('body-parser');
 
-
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartserver = async ()=>{
 
@@ -10,7 +10,9 @@ const setupAndStartserver = async ()=>{
     app.use(bodyParser.urlencoded({ extended : true}));
     app.use(bodyParser.json());
     app.listen(PORT , ()=>{
-        console.log(`server started at ${PORT}`);    
+        console.log(`server started at ${PORT}`); 
+        const repo = new CityRepository();
+        repo.createCity({name : "new_delhi"})
     });
 
 }
