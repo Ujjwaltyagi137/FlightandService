36777c21ea1e2a55d0368a4d2da23abfc75e16c1
Ujjwaltyagi137 <ujjwaltyagi9605@gmail.com>
2025-06-14 16:07:52 +0530

    added flights models

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const {PORT} = require('./config/serverConfig');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
-const setupAndStartserver = async ()=>{
+const db = require('./models/index');
+// const {Airplane} = require('./models/index');
 
+const setupAndStartServer = async () => {
+
+    // create the express object
     const app = express();
-    app.use(bodyParser.urlencoded({ extended : true}));
+
     app.use(bodyParser.json());
-    app.use('/api',ApiRoutes);
+    app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT , ()=>{
-        console.log(`server started at ${PORT}`);
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
     });
-
 }
-setupAndStartserver();
+
+setupAndStartServer();
